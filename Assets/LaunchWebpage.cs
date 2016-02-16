549526d1207e37e8583edbb1103b9613898bb038
Ray Altenberg <Ray Altenberg>
2016-02-16 00:11:35 -0800

    Gilmore

---
+++ Assets/LaunchWebpage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchWebpage : MonoBehaviour {
+
+	void Start() {
+		//Application.OpenURL("http://www.bart.gov/stations/elevators");
+	}
+	public void openWebpage(){
+		Application.OpenURL("http://www.bart.gov/stations/elevators");
+	}
+	public void openWebpage2(){
+		Application.OpenURL("http://www.bart.gov/stations/escalators");
+	}
+}
+
